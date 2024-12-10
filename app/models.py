aefb631bfef435b37ac43a78daaf96b76a9f6d14
juaki0315 <109316446+juaki0315@users.noreply.github.com>
2024-12-10 19:48:20 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import mongo, db
 from datetime import datetime
+from bson.objectid import ObjectId
 
 # MongoDB: Guardar cita
 def save_appointment(username, date, time):
@@ -17,9 +18,23 @@ def get_user_appointments(username):
     return list(collection.find({'username': username}))
 
 # MongoDB: Cancelar cita
+from bson.objectid import ObjectId
+
 def cancel_appointment(username, appointment_id):
-    collection = mongo.db.appointments
-    collection.delete_one({'_id': appointment_id, 'username': username})
+    try:
+        collection = mongo.db.appointments
+        # Convertir appointment_id a ObjectId
+        object_id = ObjectId(appointment_id)
+        print('El id de la cita es: ', object_id)
+        result = collection.delete_one({'_id': object_id, 'username': username})
+        
+        if result.deleted_count > 0:
+            print("Cita eliminada exitosamente.")
+        else:
+            print("No se encontró ninguna cita con ese ID y username.")
+    except Exception as e:
+        print("Error al eliminar la cita:", e)
+
 
 class ChatLog(db.Model):
     __tablename__ = 'chat_logs'
